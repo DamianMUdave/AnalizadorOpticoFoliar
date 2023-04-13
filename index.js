f7bc4741aba2e3edefbf6cd1a1bf37f8bf63cb1c
Damián <damianmudave@outlook.es>
2023-04-13 04:02:00 -0600

    coneccion esp32
    coneccion con esp32

---
+++ index.js
@@ -0,0 +1,59 @@
+const http = require('http');
+const express = require('express');
+const { Server } = require("socket.io");
+const path = require('path');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const port = 4000;
+var clientState = false;
+
+app.use(express.static(path.join("public")))
+
+app.get("/", (req,res) => {
+    res.sendFile(__dirname + "index.html")
+})
+
+app.get("/sensor", (req,res)=>{
+    res.sendFile(__dirname+"/public/datos.html");
+})
+
+
+function DataPrint(socket){
+    if(!clientState == true){
+      socket.send("No data for you sorry, check your credentials");
+    }else{
+      /* const ip = req.socket.remoteAddress; */
+      console.log('A new client Connected!');
+      socket.send('Welcome New Client!');
+      var today= new Date().toLocaleString('es-Mx', { timeZone: 'America/Mexico_City', });
+      console.log(today);
+      /* console.log(ip); */
+      socket.emit("date",today);
+    }
+  }
+
+
+io.on('connection', (socket) => {
+    clientState = true;
+    DataPrint(socket);
+    console.log('a user connected');
+    socket.on('sens_Data', (data) => {
+        var dt = JSON.stringify(data);
+        console.log(`message: ${dt}`);
+        io.emit('sensClientData',dt);
+    });
+    socket.on('disconnect', () => {
+        clientState = false;
+        console.log('user disconnect');
+    })
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+})
+
+
+
+server.listen(port, () => {
+    console.log((new Date()) + `Server is listening on port ${port}\n App listen on http://localhost:${port}`);
+}) 
--- public/js/index.js
+++ public/js/index.js
@@ -18,3 +18,22 @@ socket.on ('chat message', function(msg) {
     messages.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
 });
+
+//confirmacion de login
+
+function login() { //se ejecuta cuando se presiona el boton de login
+
+    var correo, Contraseña;
+    //verificación sin tokens
+    correo = document.getElementById('email').value;  //obtiene el valor del campo email
+    Contraseña = document.getElementById('pass').value; //obtiene el valor del campo pass
+
+    if(correo == "admin@hotmail.com" && Contraseña == "12345678"){ //se comprueba si el correo y la contraseña son correctos
+        location.href = "datos.html";
+        alert("Iniciaste Sesión");
+    }else{ //se comprueba si el correo y la contraseña son incorrectos
+        location.href = "index.html";
+        alert("Usuario o contraseña incorrectos");
+    }
+    
+    }
