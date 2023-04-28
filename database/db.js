eeaef282258fae6ad8a8e51331ed95a4b185d5d4
Damián <damianmudave@outlook.es>
2023-04-28 04:52:29 -0600

    ya casi
    solo un error de syntaxis en la consulta

--- database/db.js
+++ database/db.js
@@ -1,22 +1,44 @@
-//invocamos mysql
-const mysql = require('mysql'); // necesitas instalar este complemento nmp i mysql
+//Invocamos mysql
+//Necesitas instalar este complemento nmp i mysql
+const mysql = require('mysql');
 
-//creamos la conexión
-const connection = mysql.createConnection({ //en esta parte declaras los datos de conexion, como lo son el nombre de la base de datos etc.
+//Creamos la conexión
+//En esta parte declaras los datos de conexion, como lo son el nombre de la base de datos etc.
+const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
 });
-//console.log(connection);
 
-connection.connect((error) => { //este apartado es opcional solo nos permite visualizar por conzola si se establecio la conexión
+//Este apartado es opcional solo nos permite visualizar por conzola si se establecio la conexión
+connection.connect((error) => {
     if (error) {
         console.log('Error de conexión Codigo: ' + error);
         return;
     }
-    //es opcional, podemos eliminarla.
     console.log('!DB connection');
 });
 
+
+
+connection.query('USE dron_foliar; SELECT sensor.*, minerales.`N-nitrogeno`, minerales.`P-fosforo`, minerales.`K-potasio`, minerales.`Mg-Magnesio` FROM `sensor`, `minerales` WHERE Valor=138 AND sensor.`ID-Plant` = minerales.`ID-Plant`;', (error, results) => {
+    if (error) {
+        console.log('Error en la consulta Codigo: ' + error);
+        return;
+    }
+
+    // Mostramos los datos en la tabla HTML
+    $.each(results, function (index, registro) {
+        const fila = $('<tr>');
+        fila.append($('<td>').text(registro.ID - Plant));
+        fila.append($('<td>').text(registro.Name - Plant));
+        fila.append($('<td>').text(registro.Valor));
+        fila.append($('<td>').text(registro.N - nitrogeno));
+        fila.append($('<td>').text(registro.P - fosforo));
+        fila.append($('<td>').text(registro.K - potasio));
+        fila.append($('<td>').text(registro.Mg - Magnesio));
+        $('#datos').append(fila);
+    });
+});
 module.exports = connection;
--- public/js/echo.js
+++ public/js/echo.js
@@ -5,11 +5,6 @@ var m1 = document.getElementById('m1')
 var flag = document.getElementById('flag');
 var host = document.getElementById('host');
 var sensData1 = document.getElementById('sensData1');
-var sensData2 = document.getElementById('sensData2');
-var sensData3 = document.getElementById('sensData3');
-var sensData4 = document.getElementById('sensData4');
-/*var sensData5 = document.getElementById('sensData5');
-var sensData6 = document.getElementById('sensData6');*/
 
 socket.on('date', (data) => {
     messages.innerHTML = data
@@ -26,13 +21,70 @@ socket.on('sensClientData', (data) => {
     Jsonobj(data);
 })
 
-function Jsonobj(obj){
+function Jsonobj(obj) {
     var myObj = JSON.parse(obj);
     console.log(myObj);
     sensData1.innerHTML = myObj.sens1;
-    sensData2.innerHTML = myObj.sens2;
-    sensData3.innerHTML = myObj.sens3;
-    sensData4.innerHTML = myObj.sens4;
-/*    sensData5.innerHTML = myObj.sens5;
-    sensData6.innerHTML = myObj.sens6;*/
 }
+
+/*$(document).ready(function () {
+    $.ajax({
+        url: "./database/datos.php", // Archivo PHP que obtiene los datos
+        type: "GET", // Método de petición HTTP
+        success: function (data) {
+            var datos = JSON.parse(data);
+            var tbody = $("#tabla tbody");
+
+            $.each(datos, function (index, registro) {
+                var fila = $("<tr></tr>");
+                fila.append($("<td></td>").text(registro.ID-Plant));
+                fila.append($("<td></td>").text(registro.Name-Plant));
+                fila.append($("<td></td>").text(registro.Valor));
+                fila.append($("<td></td>").text(registro.N-nitrogeno));
+                fila.append($("<td></td>").text(registro.P-fosforo));
+                fila.append($("<td></td>").text(registro.K-potasio));
+                fila.append($("<td></td>").text(registro.Mg-Magnesio));
+                tbody.append(fila);
+            });
+        }
+    });
+});
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+connection.connect((error) => {
+    if (error) {
+        console.log('Error de conexión Codigo: ' + error);
+        return;
+    }
+    console.log('!DB connection');
+});
+
+connect.query('SELECT sensor.*, minerales.`N-nitrogeno`, minerales.`P-fosforo`, minerales.`K-potasio`, minerales.`Mg-Magnesio` FROM `sensor`, `minerales` WHERE Valor = 138 and sensor.`ID-Plant` = minerales.`ID-Plant`;', (error, results) => {
+    if (error) {
+        console.log('Error en la consulta Codigo: ' + error);
+        return;
+    }
+
+    // Mostramos los datos en la tabla HTML
+    $.each(results, function (index, registro) {
+        const fila = $('<tr>');
+        fila.append($('<td>').text(registro.ID - Plant));
+        fila.append($('<td>').text(registro.Name - Plant));
+        fila.append($('<td>').text(registro.Valor));
+        fila.append($('<td>').text(registro.N - nitrogeno));
+        fila.append($('<td>').text(registro.P - fosforo));
+        fila.append($('<td>').text(registro.K - potasio));
+        fila.append($('<td>').text(registro.Mg - Magnesio));
+        $('#datos').append(fila);
+    });
+});
+
+connection.end();*/
